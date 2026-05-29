fe99fd788b48405a26b66e55955b566ce987bdca
Bryansvision <144342080+Bryansvision@users.noreply.github.com>
2026-05-29 15:58:50 +0200

    AAAAAAAAAAAAAAAA

--- Assets/scripts/Touch.cs
+++ Assets/scripts/Touch.cs
@@ -2,14 +2,16 @@ using UnityEngine;
 
 public class Touch : MonoBehaviour
 {
+    public bool alreadyHit = false;
 
     public AudioSource note;
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "hands")
+        if(other.gameObject.tag == "hands" && alreadyHit == false)
         {
             note.Play();
+            alreadyHit = true;
         }
     }
 }
--- Assets/scripts/spawning discs.cs
+++ Assets/scripts/spawning discs.cs
@@ -1,16 +1,90 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 
-public class spawningdiscs : MonoBehaviour
+public class SpawningDiscs : MonoBehaviour
 {
-    public GameObject DiscPrefab;
-    
+    public GameObject discPrefab;
+    public GameObject spawnCube;
+    public GameObject instanceDisc;
+    public GameObject orbOne;
+    public GameObject orbTwo;
+    public GameObject orbThree;
+    public GameObject wholeRig;
+    public bool spawn = false;
+    public bool alreadyrotate = false;
+    public int rotation;
+
+    private BoxCollider cubeCollider;
+
+    void Start()
+    {
+        cubeCollider = spawnCube.GetComponent<BoxCollider>();
+    }
 
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Space))
+        if (spawn == false)
         {
-            Vector3 randomPosition  = new Vector3(Random.Range(-10, 11),5,Random.Range(-10, 11));
-            Instantiate(DiscPrefab, randomPosition,Quaternion.identity);
+            spawnCube.SetActive(true);
+
+           StartCoroutine(spawningNow());
+            rotation = Random.Range(0, 3);
         }
+
+        switch (rotation) {
+        case 0:
+                if(alreadyrotate == false)
+                wholeRig.transform.eulerAngles = new Vector3(0, 0, 90);
+                alreadyrotate = true;
+                break;
+        case 1:
+                if(alreadyrotate == false)
+                    wholeRig.transform.eulerAngles = new Vector3(0, 0, 180);
+                alreadyrotate = true;
+                break;
+            case 3:
+                if(alreadyrotate == false)
+                    wholeRig.transform.eulerAngles = new Vector3(0, 0, 270);
+                break;
+        }
+        
+    }
+
+    void SpawningDisc()
+    {
+        Bounds bounds = cubeCollider.bounds;
+
+        Vector3 spawnPosition = bounds.center;
+
+        instanceDisc = Instantiate(discPrefab, spawnPosition, Quaternion.identity);
+
+        instanceDisc.transform.Rotate(90, 0, 0);
+    }
+
+    IEnumerator spawningNow()
+    {
+        spawn = true;
+        wholeRig.SetActive(true);
+        orbOne.SetActive(false);
+        orbTwo.SetActive(false);
+        orbThree.SetActive(false);
+        yield return new WaitForSeconds(1);
+        spawnCube.SetActive(true);
+        orbOne.SetActive(true);
+        yield return new WaitForSeconds(1);
+        orbTwo.SetActive(true);
+        yield return new WaitForSeconds(1);
+        orbThree.SetActive(true);
+        yield return new WaitForSeconds(1);
+        SpawningDisc();
+        yield return new WaitForSeconds(5);
+        spawn = false;
+        alreadyrotate = false;
+        Destroy(instanceDisc);
+        wholeRig.SetActive(false);
+        spawn = false;
+        yield break;
     }
 }
